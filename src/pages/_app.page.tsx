d93762c83decf2740f11b03abf597fa663735bbe
Paul Bokelman <paul.bokelman1@gmail.com>
2024-08-03 20:22:49 -0700

    task: added progress bar

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -1,8 +1,19 @@
 import { ExtendedAppProps } from "~/types";
+import * as React from "react";
+import Router from "next/router";
+import NProgress from "nprogress";
 import "~/styles/globals.css";
 import { Layout } from "~/partials";
 
+NProgress.configure({ easing: "ease", speed: 500 });
+
 const AtmosphereShowcase = ({ Component, pageProps }: ExtendedAppProps) => {
+  React.useEffect(() => {
+    Router.events.on("routeChangeStart", () => NProgress.start());
+    Router.events.on("routeChangeComplete", () => NProgress.done());
+    Router.events.on("routeChangeError", () => NProgress.done());
+  }, []);
+
   return (
     <Layout {...pageProps.config}>
       <Component {...pageProps} />
